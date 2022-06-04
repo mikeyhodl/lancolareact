61b56606012d2cdbe2d236384a63b364904091b7
mikey <mike.owino40@yahoo.com>
2022-06-04 10:28:00 +0000

    ....

---
+++ src/pages/RouterPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {BrowserRouter as router, Switch, Route} from 'react-router-dom'
+
+export default function RouterPage() {
+  return (
+    <div>RouterPage</div>
+  )
+}
