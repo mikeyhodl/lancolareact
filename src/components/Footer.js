de69df11c55da3f894a3a80326cf2f6a9df9e017
mikey <mike.owino40@yahoo.com>
2022-06-06 12:47:28 +0000

    ...

---
+++ src/components/Footer.js
@@ -0,0 +1,126 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div class="section footer-section footer-section-2" style="background-image: url(assets/images/bg/footer-bg-2.jpg);">
+
+            <div class="container">
+                {/* <!-- Footer Widget Wrap Start --> */}
+                <div class="footer-widget-wrap footer-widget-wrap-2">
+                    <div class="row">
+                        <div class="col-lg-3 col-sm-6">
+                            {/* <!-- Footer Widget Start --> */}
+                            <div class="footer-widget-about">
+                                <a class="footer-logo" href="./"><img src="assets/images/logo.png" alt="Logo"/></a>
+                                <div class="widget-info widget-info-2">
+                                    <ul>
+                                        <li>
+                                            <div class="info-icon">
+                                                <i class="flaticon-phone-call"></i>
+                                            </div>
+                                            <div class="info-text">
+                                                <span><a href="tel:254115588872">+254 115 588 872</a></span>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="info-icon">
+                                                <i class="far fa-envelope-open"></i>
+                                            </div>
+                                            <div class="info-text">
+                                                <span><a href="mailto:info@lancolatech.co.ke">info@lancolatech.co.ke</a></span>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="info-icon">
+                                                <i class="flaticon-pin"></i>
+                                            </div>
+                                            <div class="info-text">
+                                                <span>Grand Pri 3rd Floor Room 301, Eldoret</span>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            {/* <!-- Footer Widget End --> */}
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            {/* <!-- Footer Widget Start --> */}
+                            <div class="footer-widget footer-widget-2">
+                                <h4 class="footer-widget-title">Useful Links</h4>
+
+                                <div class="widget-link">
+                                    <ul class="link">
+                                        <li><a href="terms-and-conditions.txt" target="_blank">Terms & Conditions</a></li>
+                                        <li><a href="about.html">About Company</a></li>
+                                        <li><a href="privacy-policy.html">Policy</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            {/* <!-- Footer Widget End --> */}
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            {/* <!-- Footer Widget Start --> */}
+                            <div class="footer-widget footer-widget-2">
+                                <h4 class="footer-widget-title">Our Services</h4>
+
+                                <div class="widget-link">
+                                    <ul class="link">
+                                        <li><a href="bulk-sms.html">Bulk SMS</a></li>
+                                        <li><a href="management-systems.html">Managment Systems</a></li>
+                                        <li><a href="computer-solutions.html">Computer Solutions</a></li>
+                                        <li><a href="digital-marketing.html">Digital Marketing</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            {/* <!-- Footer Widget End --> */}
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            {/* <!-- Footer Widget Start --> */}
+                            <div class="footer-widget footer-widget-2">
+                                <h4 class="footer-widget-title">Support</h4>
+
+                                <div class="widget-link">
+                                    <ul class="link">
+                                        <li><a href="support.html">Support</a></li>
+                                        <li><a href="faqs.html">FAQS</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            {/* <!-- Footer Widget End --> */}
+                        </div>
+                    </div>
+                </div>
+                {/* <!-- Footer Widget Wrap End --> */}
+            </div>
+
+            {/* <!-- Footer Copyright Start --> */}
+            <div class="footer-copyright-area footer-copyright-2">
+                <div class="container">
+                    <div class="footer-copyright-wrap">
+                        <div class="row align-items-center">
+                            <div class="col-lg-6 col-md-6">
+                                {/* <!-- Footer Copyright Text Start --> */}
+                                <div class="copyright-text">
+                                    <p> &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://lancolatech.co.ke/" target="_blank">Lancola Tech Company</a></p>
+                                </div>
+                                {/* <!-- Footer Copyright Text End --> */}
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                {/* <!-- Footer Copyright Social Start --> */}
+                                <div class="copyright-social">
+                                    <ul class="social">
+                                        <li><a href="https://www.facebook.com/Lancola-Tech-246152853989129" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    </ul>
+                                </div>
+                                {/* <!-- Footer Copyright Social End --> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- Footer Copyright End --> */}
+        </div>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,96 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div id="header" class="section header-section header-section-2 transparent-header">
+            <div class="container">
+
+                {/* <!-- Header Wrap Start  --> */}
+                <div class="header-wrap">
+
+                    <div class="header-logo">
+                        <a href="./"><img src="assets/images/logo.png" alt=""/></a>
+                    </div>
+
+                    <div class="header-menu header-menu-2 d-none d-lg-block">
+                        <ul class="main-menu">
+                            <li class="active-menu">
+                                <a href="./">Home</a>
+                                {/* <!-- <ul class="sub-menu">
+                                    <li><a href="./">Home One</a></li>
+                                    <li class="active"><a href="index-2.html">Home Two</a></li>
+                                    <li><a href="index-3.html">Home Three</a></li>
+                                    <li><a href="index-4.html">Home Four</a></li>
+                                    <li><a href="index-5.html">Home Five</a></li>
+                                    <li><a href="index-6.html">Home Six</a></li>
+                                </ul> --> */}
+                            </li>
+                            <li>
+                                <a href="about.html">About Us</a>
+                            </li>
+                            <li><a href="javascript:;">Our Services</a>
+                                <ul class="sub-menu">
+                                    <li><a href="bulk-sms.html">Bulk SMS</a></li>
+                                    <li><a href="management-systems.html">Management Systems</a></li>
+                                    <li><a href="computer-solutions.html">Computer Solutions</a></li>
+                                    <li><a href="computer-accessories.html">Computer Accessories</a></li>
+                                    <li><a href="CCTV-installations.html">CCTV Installtions</a></li>
+                                    <li><a href="structured-cabling-and-networking.html">Structured Cabling and Networking</a></li>
+                                    <li><a href="digital-marketing.html">Digital Marketing</a></li>
+                                    <li><a href="web-development.html">Web Development</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="https://blog.lancolatech.co.ke" target="_blank">Blog</a>
+                                {/* <!-- <ul class="sub-menu">
+                                    <li><a href="blog.html">Blog Grid</a></li>
+                                    <li><a href="blog-standard.html">Latest News</a></li>
+                                    <li><a href="blog-details.html">Blog Details</a></li>
+                                </ul> --> */}
+                            </li>
+                            <li><a href="team.html">Our Team</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+
+                    {/* <!-- Header Meta Start --> */}
+                    <div class="header-meta">
+                        {/* <!-- Header Search Start -->
+                        <!-- Header Search End -->
+
+                        <!-- Header Cart Start -->
+                        
+                        <!-- Header Cart End -->
+                        <!-- Header Info Start --> */}
+                        <div class="header-info d-none d-md-flex">
+                            <div class="info-icon">
+                                <i class="fas fa-phone-alt"></i>
+                            </div>
+                            <div class="info-text">
+                                <span>For Enquiries</span>
+                                <span class="number"><a href="tel:254115588872">+254 115 588 872</a></span>
+                                <span class="number"><a href="tel:254715223428">+254 715 223 428</a></span>
+                            </div>
+                        </div>
+                        {/* <!-- Header Info End --> */}
+                        <div class="header-btn-2 d-none d-xl-block">
+                            <a class="btn" href="mailto:info@lancolatech.co.ke" target="_blank">Email Us!</a>
+                        </div>
+                        {/* <!-- Header Toggle Start --> */}
+                        <div class="header-toggle d-lg-none">
+                            <button data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </button>
+                        </div>
+                        {/* <!-- Header Toggle End --> */}
+                    </div>
+                    {/* <!-- Header Meta End  --> */}
+
+                </div>
+                {/* <!-- Header Wrap End  --> */}
+
+            </div>
+        </div>
+  )
+}
