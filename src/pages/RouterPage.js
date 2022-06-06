9eaff9b1fe83efb6f3d86af1840cb72322fb85fd
mikey <mike.owino40@yahoo.com>
2022-06-06 06:39:05 +0000

    ....

--- src/pages/RouterPage.js
+++ src/pages/RouterPage.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import {BrowserRouter as router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 
 export default function RouterPage() {
   return (
     <div>
-        <router>
-            <switch>
-                <Router path="/" exact component={Home} />
+        <Router>
+            < Routes>
+                <Route path="/" exact component={Home} />
                 <Route path="/about" exact component={About} />
-            </switch>
-        </router>
+            </ Routes>
+        </Router>
     </div>
   )
 }
